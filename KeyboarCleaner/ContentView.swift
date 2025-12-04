f32ff9118245177180eb87acda6b575c54891b09
Mateusz <mateusz.rybczynski@icloud.com>
2025-12-04 17:35:59 +0100

    Initial Commit

---
+++ KeyboarCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyboarCleaner
+//
+//  Created by Mateusz Rybczyński on 12/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyboarCleaner/KeyboarCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboarCleanerApp.swift
+//  KeyboarCleaner
+//
+//  Created by Mateusz Rybczyński on 12/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct KeyboarCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
