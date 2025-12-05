4e0619f6230ea2e65534f907b109dad409131dcf
Mateusz <mateusz.rybczynski@icloud.com>
2025-12-05 12:36:23 +0100

    UX/UI created

--- KeyboarCleaner/ContentView.swift
+++ KeyboarCleaner/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var blocker = KeyboardBlocker()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Image(systemName: blocker.isBlocking ? "keyboard.fill" : "keyboard")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .foregroundColor(blocker.isBlocking ? .red : .green)
+                .symbolEffect(.bounce, value: blocker.isBlocking)
+            Text(blocker.isBlocking ? "Tryb czyszczenia aktywny" : "Tryb czyszczenia wyłączony")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(blocker.isBlocking ? .red : .green)
+            Text("Gdy tryb jest aktywny, klawiatura jest wyłączona.\nUżyj myszki/gładzika, aby wyłączyć blokadę.")
+                .multilineTextAlignment(.center)
+                .font(.caption)
+            
+            Button {
+                if blocker.isBlocking {
+                    blocker.stopBlocking()
+                } else {
+                    blocker.startBlocking()
+                }
+            } label: {
+                Text(blocker.isBlocking ? "Włącz klawiaturę" : "Zablokuj klawiaturę")
+                    .font(.headline)
+                    .padding()
+                    .frame(width: 200)
+                    .background(blocker.isBlocking ? Color.green : Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+            }
+            .buttonStyle(.plain)
+
         }
-        .padding()
+        .padding(50)
+        .frame(minWidth: 400, minHeight: 300)
     }
 }
 
