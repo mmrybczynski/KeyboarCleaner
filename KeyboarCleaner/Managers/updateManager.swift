0fb5ef0ab9cbf063b5602aba1976e1db64864135
Mateusz <mateusz.rybczynski@icloud.com>
2026-06-05 10:53:49 +0200

    Update manager added to project

--- KeyboarCleaner/Managers/updateManager.swift
+++ KeyboarCleaner/Managers/updateManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+internal import Combine
+
+struct AppConfig: Codable {
+    let appVersion: String
+}
+
+class UpdateManager: ObservableObject {
+    @Published var appVersion: String = ""
+    private let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+    
+    func checkForUpdates() async -> Bool {
+        guard let url = URL(string: "https://m-rybczynski.com/applications/keyclean/main.json") else {
+            return false
+        }
+        
+        do {
+            let(data, _) = try await URLSession.shared.data(from: url)
+            let config = try JSONDecoder().decode(AppConfig.self, from: data)
+            
+            await MainActor.run {
+                self.appVersion = config.appVersion
+            }
+            
+            print("Wersja z serwera: \(config.appVersion)")
+            
+            if config.appVersion != version {
+                return true
+            } else {
+                return false
+            }
+        } catch {
+            print("Błąd \(error)")
+            return false
+        }
+    }
+    
+}
